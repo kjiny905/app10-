023f73641af3e739dffca8fab86c918475365461
pkj <kjiny905217@gmail.com>
2023-03-29 11:33:36 +0900

    230329-1

---
+++ src/main/java/com/kh/app/web/form/member/JoinForm.java
@@ -0,0 +1,15 @@
+package com.kh.app.web.form.member;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class JoinForm {
+  private String email;
+  private String passwd;
+  private String nickname;
+  private String gender;
+  private List<String> hobby;
+  private String region;
+}
